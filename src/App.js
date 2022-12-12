8eeab4cd1dece91757d951a3200d58fd6cc00343
AbhishekRai48 <abhishek.rai@cantire.com>
2022-12-13 01:58:00 +0530

    add websocket to client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React, {useState} from 'react';
+import { createSocketConnection } from "./sockets/socketEvents";
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [socketId, setSocketId] = useState('');
+
+  const connect = () => {
+    const socket = createSocketConnection();
+    socket.on('connect', () => {
+      setConnected(socket.connected);
+      setSocketId(socket.id);
+    })
+  }
+
   return (
     <div className="App">
-      
+      <button onClick={connect}>Connect</button>
+      <div>{connected ? `user connected ${socketId}`: `Failed to connect`}</div>
     </div>
   );
 }
